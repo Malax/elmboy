7a45aa384b430cc039fd8751d44b56d40277ae44
Manuel Fuchs <malax@malax.de>
2019-01-02 13:02:54 +0100

    Add missing import

--- src/elm/Ports.elm
+++ src/elm/Ports.elm
@@ -1,5 +1,7 @@
 port module Ports exposing (queueAudioSamples, setPixelsFromBatches)
 
+import Array exposing (Array)
+
 
 port setPixelsFromBatches : { canvasId : String, pixelBatches : List Int } -> Cmd msg
 
