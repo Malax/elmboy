76601785dc75ebe74f8fd83d7ca20803a986dcd3
Manuel Fuchs <malax@malax.de>
2019-02-03 21:52:20 +0100

    Replace scrolling issue fix

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,15 @@
 import { Elm } from '../elm/Main.elm'
 
 document.addEventListener('DOMContentLoaded', function (event) {
+  // There is currently no way to prevent default for Browser.Events in Elm. I took the pragmatic approach here to just prevent default
+  // on the error keys whose default is problematic due to the fact they might scroll the viewport while playing. This should be replaced as
+  // soon as we can prevent default for Browser.Events.
+  document.addEventListener('keydown', function (e) {
+    if (e.key === 'ArrowDown' || e.key === 'ArrowUp' || e.key === 'ArrowLeft' || e.key === 'ArrowRight') {
+      e.preventDefault()
+    }
+  })
+
   const app = Elm.Main.init({
     node: document.getElementById('elmboy')
   })
